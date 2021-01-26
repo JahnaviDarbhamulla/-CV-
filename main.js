f714172222776f2be51b0b41d75ac44b02939b8a
JahnaviDarbhamulla <76477365+JahnaviDarbhamulla@users.noreply.github.com>
2021-01-26 11:50:13 +0530

    add initial files

---
+++ main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+  "use strict"
+
+
+  $(function() {
+    $(".p2").typed({
+      strings: ["DESIGNER", "DEVELOPER", "CREATIVE"],
+      typeSpeed: 50,
+      backSpeed: 10,
+      backDelay: 2000,
+      showCursor: false,
+      loop: false
+    });
+  });
